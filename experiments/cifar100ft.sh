78e516853d70c1a54996031841e049609bc033d7
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2024-03-01 11:04:35 +0100

    experiments scripts for ewc and ft

---
+++ experiments/cifar100ft.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH --ntasks=3   # number of processor cores (i.e. tasks)
+#SBATCH --gpus=1
+
+set -e
+
+eval "$(conda shell.bash hook)"
+conda activate FACIL
+
+num_tasks=10
+nc_first_task=10
+dataset=cifar100_icarl
+network=resnet32
+tag=cifar100t${num_tasks}s${nc_first_task}
+
+seed=0
+num_epochs=100
+lr=0.1
+wu_lr=0.8
+
+for wu_nepochs in 0 20; do
+  ./experiments/ft1.sh 0 ${seed} ${tag} ${dataset} ${num_tasks} ${nc_first_task} ${network} ${num_epochs} ${wu_nepochs} ${wu_lr} ${lr} &
+done
+wait
+
+head_init=zeros
+
+for wu_nepochs in 0 20; do
+  ./experiments/ft2.sh 0 ${seed} ${tag} ${dataset} ${num_tasks} ${nc_first_task} ${network} ${num_epochs} ${wu_nepochs} ${wu_lr} ${lr} ${head_init} &
+done
+wait
---
+++ experiments/ewc2.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e
+
+gpu=$1
+seed=$2
+tag=$3
+dataset=$4
+num_tasks=$5
+nc_first_task=$6
+network=$7
+num_epochs=$8
+wu_epochs=${9:-0}
+wu_lr=${10:-0.1}
+lr=${11:-0.1}
+head_init=${12}
+
+if [ "${dataset}" = "imagenet_subset_kaggle" ]; then
+  clip=1.0
+else
+  clip=100.0
+fi
+
+if [ ${wu_epochs} -gt 0 ]; then
+  exp_name="${tag}:wu"
+  result_path="results/${tag}/ewc_wu_${seed}"
+  python3 src/main_incremental.py \
+    --exp-name ${exp_name} \
+    --gpu ${gpu} \
+    --datasets ${dataset} \
+    --num-tasks ${num_tasks} \
+    --nc-first-task ${nc_first_task} \
+    --network ${network} \
+    --use-test-as-val \
+    --lr ${lr} \
+    --clipping ${clip} \
+    --nepochs ${num_epochs} \
+    --batch-size 128 \
+    --seed ${seed} \
+    --log disk wandb \
+    --results-path ${result_path} \
+    --tags ${tag} \
+    --approach ewc \
+    --wu-nepochs ${wu_epochs} \
+    --wu-lr ${wu_lr} \
+    --wu-fix-bn \
+    --wu-scheduler cosine \
+    --head-init-mode ${head_init}
+else
+  exp_name="${tag}:lamb_${lamb}"
+  result_path="results/${tag}/ewc_${seed}"
+  python3 src/main_incremental.py \
+    --exp-name ${exp_name} \
+    --gpu ${gpu} \
+    --datasets ${dataset} \
+    --num-tasks ${num_tasks} \
+    --nc-first-task ${nc_first_task} \
+    --network ${network} \
+    --use-test-as-val \
+    --lr ${lr} \
+    --clipping ${clip} \
+    --nepochs ${num_epochs} \
+    --batch-size 128 \
+    --seed ${seed} \
+    --log disk wandb \
+    --results-path ${result_path} \
+    --tags ${tag} \
+    --approach ewc \
+    --head-init-mode ${head_init}
+fi
