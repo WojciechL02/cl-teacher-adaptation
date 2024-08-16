ad94357ac8ad5146480d4b1aa15ef1fd2690058d
Filip Szatkowski <fmszatkowski@gmail.com>
2024-08-16 11:21:22 +0200

    Add code for plot 1

---
+++ plots/constants.py
@@ -0,0 +1,18 @@
+NAME_FT = "Finetuning"
+NAME_NMC_FULL = "NMC(Full)"
+NAME_NMC_EX = "NMC(Exemplars)"
+
+COLOR_PALETTE = {
+    NAME_FT: "tab:red",
+    NAME_NMC_FULL: "tab:green",
+    NAME_NMC_EX: "tab:blue",
+}
+HUE_ORDER = {
+    NAME_FT: 1,
+    NAME_NMC_EX: 2,
+    NAME_NMC_FULL: 3,
+}
+PLOT_LINEWIDTH = 1
+TICK_FONTSIZE = 14
+TEXT_FONTSIZE = 16
+LEGEND_FONTSIZE = 14
